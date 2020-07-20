2c36a02610b4d58475f78b3c14c10c338c0ddac5
manimaniz <manimaniz>
2020-07-20 07:19:32 +0530

    fist project

--- src/test/java/org/sample1/MyPoject.java
+++ src/test/java/org/sample1/MyPoject.java
@@ -1,5 +1,24 @@
 package org.sample1;
 
+import org.testng.annotations.Test;
+
 public class MyPoject {
+	@Test
+	private void tc11() {
+		System.out.println("tc11"+ Thread.currentThread().getId() );
+	}
+
+	@Test
+	private void tc22() {
+		
+		System.out.println("tc22"+ Thread.currentThread().getId() );
+
+	}
+
+	@Test
+	private void tc33() {
+		System.out.println("tc33"+ Thread.currentThread().getId() );
+
+	}
 
 }
